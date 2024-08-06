1ed5839245502ea173538cf4612b9ad8c1956ecc
boburastonov <boburastonov33@gmail.com>
2024-08-06 19:13:22 +0500

    add accordion with media styles, but with mistakes too

--- src/pages/home/accordion.tsx
+++ src/pages/home/accordion.tsx
@@ -14,42 +14,44 @@ const Accardion = () => {
   return (
     <section>
       <div className="max-w-7xl px-3 mx-auto">
-        <h1 className="text-[33px] font-semibold text-center py-[10px]">
+        <h1 className="lg:text-[33px] lg:py-[10px] md:text-[27px] md:py-8 py-5 text-[23px] font-semibold text-center">
           Mijozlar tomonidan berilgan savollar
         </h1>
-        <Accordion
-          variant="splitted"
-          className="w-[80%] mx-auto border border-solid border-[#d9d9d9] rounded-[8px] bg-[#FAFAFA]"
-        >
-          <AccordionItem
-            key="1"
-            aria-label="Sizning mahsulotlaringizning saqlash muddati qancha?"
-            title="Sizning mahsulotlaringizning saqlash muddati qancha?"
+        <div className="md:py-5 mb-5">
+          <Accordion
+            variant="splitted"
+            className="w-[80%] mx-auto border border-solid border-[#d9d9d9] rounded-[8px] bg-[#FAFAFA]"
           >
-            {Content1}
-          </AccordionItem>
-          <AccordionItem
-            key="2"
-            aria-label="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?"
-            title="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?"
-          >
-            {Content2}
-          </AccordionItem>
-          <AccordionItem
-            key="3"
-            aria-label="Qanday tarkibiy qismlar ishlatiladi?"
-            title="Qanday tarkibiy qismlar ishlatiladi?"
-          >
-            {Content3}
-          </AccordionItem>
-          <AccordionItem
-            key="4"
-            aria-label="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
-            title="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
-          >
-            {Content4}
-          </AccordionItem>
-        </Accordion>
+            <AccordionItem
+              key="1"
+              aria-label="Sizning mahsulotlaringizning saqlash muddati qancha?"
+              title="Sizning mahsulotlaringizning saqlash muddati qancha?"
+            >
+              {Content1}
+            </AccordionItem>
+            <AccordionItem
+              key="2"
+              aria-label="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?"
+              title="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?"
+            >
+              {Content2}
+            </AccordionItem>
+            <AccordionItem
+              key="3"
+              aria-label="Qanday tarkibiy qismlar ishlatiladi?"
+              title="Qanday tarkibiy qismlar ishlatiladi?"
+            >
+              {Content3}
+            </AccordionItem>
+            <AccordionItem
+              key="4"
+              aria-label="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
+              title="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
+            >
+              {Content4}
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
     </section>
   );
