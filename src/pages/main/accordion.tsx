ed899bdeb68d74c5a3c45ea51b1a7721e531cf57
boburastonov <boburastonov33@gmail.com>
2024-08-09 17:09:12 +0500

    add language function to accordion section

--- src/pages/main/accordion.tsx
+++ src/pages/main/accordion.tsx
@@ -1,78 +1,39 @@
 "use client";
 import React from "react";
 import Accordion from "./accordionItem";
+import { useTranslations } from "next-intl";
 
 const Accardion = () => {
+  const t = useTranslations()
   return (
     <section>
       <div className="max-w-7xl px-3 mx-auto">
         <h1 className="lg:text-[33px] lg:py-[10px] md:text-[27px] md:py-8 py-5 text-[23px] font-semibold text-center">
-          Mijozlar tomonidan berilgan savollar
+          {t('savollar')}
         </h1>
         <div className="md:py-5 mb-5">
-          {/* <Accordion
-            variant="splitted"
-            className="w-[80%] mx-auto border border-solid border-[#d9d9d9] rounded-[8px] bg-[#FAFAFA]"
-          >
-            <AccordionItem
-              key="1"
-              aria-label="Sizning mahsulotlaringizning saqlash muddati qancha?"
-              title="Sizning mahsulotlaringizning saqlash muddati qancha?"
-            >
-              {Content1}
-            </AccordionItem>
-            <AccordionItem
-              key="2"
-              aria-label="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?"
-              title="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?"
-            >
-              {Content2}
-            </AccordionItem>
-            <AccordionItem
-              key="3"
-              aria-label="Qanday tarkibiy qismlar ishlatiladi?"
-              title="Qanday tarkibiy qismlar ishlatiladi?"
-            >
-              {Content3}
-            </AccordionItem>
-            <AccordionItem
-              key="4"
-              aria-label="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
-              title="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
-            >
-              {Content4}
-            </AccordionItem>
-          </Accordion> */}
-          <div className="min-h-screen p-8">
+          <div className="md:p-6 p-3">
             <main className="max-w-2xl mx-auto">
-              <Accordion title="Sizning mahsulotlaringizning saqlash muddati qancha?">
+              <Accordion title={t("savol1")}>
                 <p>
-                  Biz zamonaviy evropa texnologiyalari va uskunalaridan
-                  foydalanamiz, bu bizning mahsulotlarimizning yuqori sifatini
-                  kafolatlaydi.
+                  {t("kafolat")}
                 </p>
               </Accordion>
-              <Accordion title="Ishlab chiqarishda qaysi texnologiyalar qo'llaniladi?">
+              <Accordion title={t("savol2")}>
                 <p>
-                  Biz zamonaviy evropa texnologiyalari va uskunalaridan
-                  foydalanamiz, bu bizning mahsulotlarimizning yuqori sifatini
-                  kafolatlaydi.
+                  {t("kafolat")}
                 </p>
               </Accordion>
-              <Accordion title="Qanday tarkibiy qismlar ishlatiladi?">
+              <Accordion title={t("savol3")}>
                 <p>
-                  Biz zamonaviy evropa texnologiyalari va uskunalaridan
-                  foydalanamiz, bu bizning mahsulotlarimizning yuqori sifatini
-                  kafolatlaydi.
+                  {t("kafolat")}
                 </p>
               </Accordion>
               <Accordion
-                title="Sizning mahsulotlaringizni qayerdan xarid qilishim mumkin?"
+                title={t("savol4")}
               >
                 <p>
-                  Biz zamonaviy evropa texnologiyalari va uskunalaridan
-                  foydalanamiz, bu bizning mahsulotlarimizning yuqori sifatini
-                  kafolatlaydi.
+                  {t("kafolat")}
                 </p>
               </Accordion>
             </main>
