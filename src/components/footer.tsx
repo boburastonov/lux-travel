6d2a28970434dbd1b6db0c661cda0e328401c377
boburastonov <boburastonov33@gmail.com>
2024-08-09 17:08:31 +0500

    add language function to footer section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,10 @@ import { BiLogoLinkedin } from "react-icons/bi";
 import { SiMessenger } from "react-icons/si";
 import { FaTwitter, FaInfinity, FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
+import { useTranslations } from "next-intl";
 
 const Footer = () => {
+  const t = useTranslations();
   return (
     <footer>
       <div className="max-w-7xl px-3 py-5 mx-auto">
@@ -23,7 +25,7 @@ const Footer = () => {
               />
             </Link>
             <p className="text-[14px] text-[#333]">
-              O’zbekiston va qo’shni davlatlar bo’ylab tur xizmatlari
+              {t("O’zbekiston va qo’shni davlatlar bo’ylab tur xizmatlari")}
             </p>
             <div className="py-5 flex justify-center gap-[10px] text-orange-400 text-[20px]">
               <BiLogoLinkedin />
@@ -35,42 +37,79 @@ const Footer = () => {
             </div>
           </li>
           <li className="md:px-[30px] md:w-[30%] w-[90%] flex items-center flex-col justify-center md:self-start">
-            <h1 className="text-[24px] font-semibold pt-[25px]">Kompaniya</h1>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Biz haqimizda
+            <h1 className="text-[24px] font-semibold pt-[25px]">
+              {t("Kompaniya")}
+            </h1>
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Biz haqimizda")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Nega biz?
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Nega biz")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Bog’lanish
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Bog’lanish")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Xizmatlar
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Xizmatlar")}
             </a>
           </li>
           <li className="md:px-[30px] md:w-[30%] w-[90%] flex items-center flex-col justify-center md:self-start">
-            <h1 className="text-[24px] font-semibold pt-[25px]">Yo’nalishlar</h1>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Buxoro
+            <h1 className="text-[24px] font-semibold pt-[25px]">
+              {t("Yo’nalishlar")}
+            </h1>
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Buxoro")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Toshkent
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Toshkent")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Samarqand
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Samarqand")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Zomin
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Zomin")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Xiva
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Xiva")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Turkiston
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Turkiston")}
             </a>
-            <a href="#" className="text-[17px] pt-[10px] text-[#333]">
-              Surxondaryo
+            <a
+              href="#"
+              className="text-[17px] pt-[10px] text-[#333] hover:opacity-90"
+            >
+              {t("Surxondaryo")}
             </a>
           </li>
         </ul>
