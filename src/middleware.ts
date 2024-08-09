5dbbadb5268b26da8bcb20100118da3470d3fd1a
boburastonov <boburastonov33@gmail.com>
2024-08-09 19:04:33 +0500

    updatee language

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(uz|ru|en)/:path*"],
+  matcher: ["/", "/(uz|ru|eng)/:path*"],
 };
