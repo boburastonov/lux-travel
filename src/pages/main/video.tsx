df4396850ba63aece26c2f0fdda75cef65467ec4
boburastonov <boburastonov33@gmail.com>
2024-08-09 17:09:57 +0500

    add language function to video section

--- src/pages/main/video.tsx
+++ src/pages/main/video.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useTranslations } from "next-intl";
 
 const Video = () => {
+  const t = useTranslations();
   return (
     <section className="pt-5">
       <div className="max-w-7xl py-3 mx-auto">
         <h1 className="w-full lg:text-[30px] md:text-[27px] text-[23px] mb-5 font-semibold text-center">
-          Biz haqimizda quyidagi video orqali batafsil bilib oling
+          {t("video")}
         </h1>
         <div className="w-[60%] h-[400px] mx-auto flex items-center justify-center">
           <iframe
