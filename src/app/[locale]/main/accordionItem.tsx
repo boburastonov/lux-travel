d0499be5b635b32e7f2352aa31464db66a586898
boburastonov <boburastonov33@gmail.com>
2024-08-16 16:03:25 +0500

    update accordion styles

--- src/app/[locale]/main/accordionItem.tsx
+++ src/app/[locale]/main/accordionItem.tsx
@@ -17,7 +17,7 @@ const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
       >
         <div className="flex items-center justify-between">
           <span className="font-semibold">{title}</span>
-          <span>{isOpen ? "-" : "+"}</span>
+          <span className="flex items-center justify-center w-5 h-5 rounded-[50%] hover:bg-[#222] hover:text-white">{isOpen ? "-" : "+"}</span>
         </div>
       </button>
       {isOpen && <div className="p-4">{children}</div>}
