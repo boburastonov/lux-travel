85ab142de9414416f9f4355b514951bd84872b65
boburastonov <boburastonov33@gmail.com>
2024-08-17 09:29:32 +0500

    update video section styles

--- src/app/[locale]/buxoro/video.tsx
+++ src/app/[locale]/buxoro/video.tsx
@@ -7,10 +7,10 @@ const Video: React.FC = () => {
   return (
     <section className="pt-5">
       <div className="max-w-7xl py-3 mx-auto">
-        <h1 className="w-full lg:text-[30px] md:text-[27px] text-[23px] mb-5 font-semibold text-center">
+        <h1 className="w-[99%] lg:text-[30px] md:text-[27px] text-[23px] mb-5 font-semibold text-center">
           {t("video")}
         </h1>
-        <div className="w-[60%] h-[400px] mx-auto flex items-center justify-center">
+        <div className="lg:w-[60%] w-[90%] h-[400px] mx-auto flex items-center justify-center">
           <iframe
             width="100%"
             height="100%"
