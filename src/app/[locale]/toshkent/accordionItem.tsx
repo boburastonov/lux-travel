d55d6826b46376199eda24ea8c33bbc591d25a16
boburastonov <boburastonov33@gmail.com>
2024-08-17 09:05:06 +0500

    update accordion styles

--- src/app/[locale]/toshkent/accordionItem.tsx
+++ src/app/[locale]/toshkent/accordionItem.tsx
@@ -14,9 +14,9 @@ const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
         className="w-full py-4 text-left focus:outline-none"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <div className="flex items-center justify-between">
+        <div className="plus flex items-center justify-between">
           <span className="font-semibold">{title}</span>
-          <span>{isOpen ? "-" : "+"}</span>
+          <span className="plus-icon flex items-center justify-center w-5 h-5 rounded-[50%] transition-all duration-[0.3s]">{isOpen ? "-" : "+"}</span>
         </div>
       </button>
       {isOpen && <div className="p-4">{children}</div>}
