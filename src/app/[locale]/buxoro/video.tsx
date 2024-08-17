2dfc0bdbc74f34d37873fbdb44c86f3cf11fc1c8
boburastonov <boburastonov33@gmail.com>
2024-08-17 10:57:35 +0500

    update video section of bukhara page

--- src/app/[locale]/buxoro/video.tsx
+++ src/app/[locale]/buxoro/video.tsx
@@ -11,17 +11,7 @@ const Video: React.FC = () => {
           {t("video")}
         </h1>
         <div className="lg:w-[60%] w-[90%] h-[400px] mx-auto flex items-center justify-center">
-          <iframe
-            width="100%"
-            height="100%"
-            className="rounded-[10px]"
-            src="https://www.youtube.com/embed/48PSJqwZVIc?si=XpYJWX7WdxQeWOc7"
-            title="YouTube video player"
-            frameBorder={0}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
+        <iframe width="100%" height="100%" className="rounded-[10px]" src="https://www.youtube.com/embed/BlCxQ4Ci1qg?si=7eEqYQdUwVYwmsEK" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
       </div>
     </section>
