cb4a71d35c34fe73dc011ecd1d25624bcb2061df
boburastonov <boburastonov33@gmail.com>
2024-08-25 11:14:25 +0500

    update video section and add hero background image to tashkent page

--- src/app/[locale]/toshkent/page.tsx
+++ src/app/[locale]/toshkent/page.tsx
@@ -11,7 +11,7 @@ import Contact from "./contact";
 const Main: React.FC = () => {
   return (
     <>
-      <main className="main">
+      <main className="tashkent">
         <Header />
         <Hero />
       </main>
